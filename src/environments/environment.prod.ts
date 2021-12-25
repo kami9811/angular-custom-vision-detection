1c47eeb2580350ec0ae00a7e40ad43ee93e5f668
yuta.kawakami <kami.981108@gmail.com>
2021-12-25 09:46:16 +0900

    Env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  cv_key: "9339a37da7c94b028eac0841658a36ce",  // top page
-  cv_endpoint: "https://jpyutanitkhack-prediction.cognitiveservices.azure.com/",  // top page
-  project_id: "5dc2c406-eecb-43c1-b926-fac6fee7fe15",  // project page
+  cv_key: "CV_KEY",  // top page
+  cv_endpoint: "CV_ENDPOINT",  // top page
+  project_id: "PROJECT_PAGE",  // project page
   published_name: "model",  // when publishing time
 };
